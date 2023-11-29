a28402a2f54e8c6779ef611ad905b111173b432b
Marlon <61248995+bedimcode@users.noreply.github.com>
2023-11-29 12:22:36 -0500

    Comments are modified

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,14 @@ const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
 
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
+/* Menu show */
 if(navToggle){
     navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu')
     })
 }
 
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
+/* Menu hidden */
 if(navClose){
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
