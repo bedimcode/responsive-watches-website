1fd7cf6e6489db6aba7398a6606a12a8b9a9d2f5
Marlon Pilco (Bedimcode) <bedimcode@gmail.com>
2026-01-09 18:45:46 -0500

    Optimize scroll sections active link function Edit comments and sort functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,38 @@
-/*=============== SHOW MENU ===============*/
+/*=============== SHOW & CLOSE MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
 
-/* Menu show */
+/* Show menu */
 if(navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+   })
 }
 
-/* Menu hidden */
+/* Hide menu */
 if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
-    })
+   navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
 }
 
-/*=============== REMOVE MENU MOBILE ===============*/
+/*=============== REMOVE MOBILE MENU ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
 const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
+   const navMenu = document.getElementById('nav-menu')
+   // When we click on each nav__link, we remove the show-menu class
+   navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*=============== CHANGE BACKGROUND HEADER ===============*/
+/*=============== CHANGE HEADER STYLES ===============*/
 const scrollHeader = () =>{
-    const header = document.getElementById('header')
-    // Add a class if the bottom offset is greater than 50 of the viewport
-    this.scrollY >= 50 ? header.classList.add('scroll-header') 
-                       : header.classList.remove('scroll-header')
+   const header = document.getElementById('header')
+   // Add the .scroll-header class if the bottom scroll of the viewport is greater than 50
+   this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                      : header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
@@ -65,35 +65,36 @@ let newSwiper = new Swiper(".new-swiper", {
     },
 });
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]')
-    
-const scrollActive = () =>{
-  	const scrollDown = window.scrollY
-
-	sections.forEach(current =>{
-		const sectionHeight = current.offsetHeight,
-			  sectionTop = current.offsetTop - 58,
-			  sectionId = current.getAttribute('id'),
-			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
-
-		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-			sectionsClass.classList.add('active-link')
-		}else{
-			sectionsClass.classList.remove('active-link')
-		}                                                    
-	})
-}
-window.addEventListener('scroll', scrollActive)
-
 /*=============== SHOW SCROLL UP ===============*/ 
 const scrollUp = () =>{
 	const scrollUp = document.getElementById('scroll-up')
-    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+   // Add the .scroll-header class if the bottom scroll of the viewport is greater than 350
 	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-						: scrollUp.classList.remove('show-scroll')
+						     : scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+// Link the ID of each section (section id="home") to each link (a href="#home") 
+// and activate the link with the class .active-link
+const scrollActive = () => {
+   // We get the position by scrolling down
+   const scrollY = window.scrollY
+
+   sections.forEach(section => {
+      const id = section.id, // id of each section
+            top = section.offsetTop - 50, // Distance from the top edge
+            height = section.offsetHeight, // Element height
+            link = document.querySelector('.nav__menu a[href*=' + id + ']') // id nav link
+
+      if(!link) return
+
+      link.classList.toggle('active-link', scrollY > top && scrollY <= top + height)
+   })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW CART ===============*/
 const cart = document.getElementById('cart'),
